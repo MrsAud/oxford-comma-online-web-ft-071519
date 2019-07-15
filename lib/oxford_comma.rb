8eef98c47b780582a27ab806b1f1653624121fb0
Audrea Cook <audreacwork@gmail.com>
2019-07-15 19:29:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,7 +9,8 @@ def oxford_comma(array)
     when 3
       puts "#{array[0]}, #{array[1]} and #{array[2]}"
     when >= 4
-      
+      list = array[0...-1].join(", ")
+      puts "#{list}, and #{array[-1]}"
     end
   binding.pry
 end
