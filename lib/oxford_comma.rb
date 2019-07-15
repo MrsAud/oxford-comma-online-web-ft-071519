739969d28f9ece36292d44aa9a97263d839b9f5d
Audrea Cook <audreacwork@gmail.com>
2019-07-15 19:38:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  case array.size
+    when 1
+      array[0]
+    when 2
+      "#{array[0]} and #{array[1]}"
+    when 3
+      "#{array[0]}, #{array[1]}, and #{array[2]}"
+    else
+      list = array[0...-1].join(", ")
+      "#{list}, and #{array[-1]}"
+    end
 end
