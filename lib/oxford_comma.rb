db57c448e3c2df3505d0fefd07c35b8e721f44e1
Audrea Cook <audreacwork@gmail.com>
2019-07-15 19:20:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def oxford_comma(array)
-  case 
+  new_array = array.split
+  #case new_array.length
+  #  when 1
+      
+  #  when 2
+      
+  #  when 3
+      
+  #  when >= 4
+      
+  #  end
+  binding.pry
 end
